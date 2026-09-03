d129907d529c04ddf20bc5b233e5f75b2343e0bc
is-leeroy-jenkins <terry.eppler@gmail.com>
2026-09-03 09:29:18 -0400

    updates

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-'''Fonky provider-neutral Minion interface.'''from __future__ import annotations
+'''Provider-neutral Minion interface.'''from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from typing import Any
